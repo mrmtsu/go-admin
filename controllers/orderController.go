d645cffdd298386035bc639741518b5d6a2a0c3e
mrmtsu <81202muramatsu@gmail.com>
2021-03-28 08:39:21 +0900

    add: order機能

---
+++ controllers/orderController.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"go-admin/database"
+	"go-admin/models"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AllOrders(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+
+	return c.JSON(models.Paginate(database.DB, &models.Order{}, page))
+}
