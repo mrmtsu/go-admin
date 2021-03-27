f24c67ca34a4449d3631a4feb35749e5581f2038
mrmtsu <81202muramatsu@gmail.com>
2021-03-27 21:32:14 +0900

    add: crud product

--- database/connect.go
+++ database/connect.go
@@ -25,5 +25,5 @@ func Connect() {
 
 	DB = database
 
-	database.AutoMigrate(&models.User{}, &models.Role{}, &models.Permission{})
+	database.AutoMigrate(&models.User{}, &models.Role{}, &models.Permission{}, &models.Product{})
 }
